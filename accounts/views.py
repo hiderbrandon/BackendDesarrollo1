94661ecd32e0ffee2b88bfc90a0879761281069f
hiderbrandon <hiderbrandon@gmail.com>
2024-04-07 21:55:48 -0500

    added and modified all CRUD Operations on aith profile

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import MyTokenObtainPairSerializer, RegisterSerializer, ProfileSerializer
+from django.shortcuts import get_object_or_404
+from .serializers import MyTokenObtainPairSerializer, UserSerializer ,AllFieldsUserSerializer
 from .models import CustomUser
 
 from django.shortcuts import render
@@ -19,21 +20,29 @@ class MyTokenObtainPairView(TokenObtainPairView):
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     permission_classes = (AllowAny,)
-    serializer_class = RegisterSerializer
+    serializer_class = UserSerializer
 
 #api/profile  and api/profile/update
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getProfile(request):
     user = request.user
-    serializer = ProfileSerializer(user, many=False)
+    serializer = AllFieldsUserSerializer(user, many=False)  # Utiliza AllFieldsUserSerializer en lugar de UserSerializer
     return Response(serializer.data)
 
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
 def updateProfile(request):
     user = request.user
-    serializer = ProfileSerializer(user, data=request.data, partial=True)
+    serializer = AllFieldsUserSerializer(user, data=request.data, partial=True)
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def deleteProfile(request):
+    user_id = request.data.get('user_id')  # Obtener la ID del usuario del cuerpo de la solicitud
+    user = get_object_or_404(CustomUser, id=user_id)
+    user.delete()
+    return Response({"message": "User deleted successfully"})
--- backend/urls.py
+++ backend/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from rest_framework_simplejwt import views as jwt_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('modulo1.urls')),  # Include your app's URLs
-    path('api-auth/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api-auth/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include('modulo1.urls')),  
+    path('api-auth/', include('accounts.urls')), 
+    
 ]
